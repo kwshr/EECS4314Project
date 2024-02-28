ef5aa70a3dd90e77fd25f1f1e38ebd2858f03f0d
Kawshar <kwshr96626@gmail.com>
2024-02-28 14:13:29 -0500

    Implement Seller Microservice

--- Backend/eauction/SellerService/seller-service/src/main/java/com/eauction/sellerservice/Seller.java
+++ Backend/eauction/SellerService/seller-service/src/main/java/com/eauction/sellerservice/Seller.java
@@ -3,6 +3,6 @@ package com.eauction.sellerservice;
 import com.common.Item;
 
 public interface Seller {
-    void uploadSellItems(Item item);
-    public void updateDutchAuctionprice(int itemId, double newprice);
+    SellerQueryResult addSellItems(Item item);
+    SellerQueryResult updateDutchAuctionprice(int itemId, double newprice);
 }
