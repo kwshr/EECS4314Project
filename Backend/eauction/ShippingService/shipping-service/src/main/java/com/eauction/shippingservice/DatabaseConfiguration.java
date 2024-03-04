19c70566918b3766c552ac3eca5cafde9231a44f
Kawshar <kwshr96626@gmail.com>
2024-03-03 21:39:38 -0500

    Add database and dependencies

---
+++ Backend/eauction/ShippingService/shipping-service/src/main/java/com/eauction/shippingservice/DatabaseConfiguration.java
@@ -0,0 +1,25 @@
+package com.eauction.shippingservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:C:/dev/E-Auction/Database/database.db");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
+
