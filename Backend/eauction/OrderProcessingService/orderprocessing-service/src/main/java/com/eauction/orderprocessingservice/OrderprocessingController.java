f35744594d68e1d8545b70e9f489e7a01d945dad
Kawshar <kwshr96626@gmail.com>
2024-03-05 17:42:13 -0500

    Fix: Controller request

--- Backend/eauction/OrderProcessingService/orderprocessing-service/src/main/java/com/eauction/orderprocessingservice/OrderprocessingController.java
+++ Backend/eauction/OrderProcessingService/orderprocessing-service/src/main/java/com/eauction/orderprocessingservice/OrderprocessingController.java
@@ -4,10 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
