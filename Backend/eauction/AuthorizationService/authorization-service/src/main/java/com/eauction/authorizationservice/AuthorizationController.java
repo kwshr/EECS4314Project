5d77c38a32f2a02ea9f074a4cb22e68e029cc962
Kawshar <kwshr96626@gmail.com>
2024-03-05 21:39:26 -0500

    Add userDetails endpoint

--- Backend/eauction/AuthorizationService/authorization-service/src/main/java/com/eauction/authorizationservice/AuthorizationController.java
+++ Backend/eauction/AuthorizationService/authorization-service/src/main/java/com/eauction/authorizationservice/AuthorizationController.java
@@ -2,6 +2,7 @@ package com.eauction.authorizationservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +24,7 @@ public class AuthorizationController {
 
     @RequestMapping(value="/signUp", method=RequestMethod.POST)
     public ResponseEntity<Map<String,Object>> signUpUser(@RequestBody User user) {
-        AuthorizationQueryResult authorizationResult = authorizationImpl.SignUp(user);
+        AuthorizationQueryResult authorizationResult = authorizationImpl.signUp(user);
         Map<String,Object> response = new HashMap<String,Object>();
         response.put("message",authorizationResult.getMessage());
         response.put("queryStatus", authorizationResult.getAuthorizationStatus());
@@ -34,7 +35,7 @@ public class AuthorizationController {
     public ResponseEntity<Map<String,Object>> signInUser(@RequestBody Map<String,String> credentials) {
        String userName = credentials.get("userName");
        String password = credentials.get("password");
-        AuthorizationQueryResult authorizationResult = authorizationImpl.SignIn(userName,password);
+        AuthorizationQueryResult authorizationResult = authorizationImpl.signIn(userName,password);
         Map<String,Object> response = new HashMap<String,Object>();
         response.put("message",authorizationResult.getMessage());
         response.put("queryStatus", authorizationResult.getAuthorizationStatus());
@@ -45,10 +46,20 @@ public class AuthorizationController {
     public ResponseEntity<Map<String,Object>> passwordReset(@RequestBody Map<String,String> credentials) {
        String userName = credentials.get("userName");
        String newPassword = credentials.get("newPassword");
-        AuthorizationQueryResult authorizationResult = authorizationImpl.PasswordReset(userName,newPassword);
+        AuthorizationQueryResult authorizationResult = authorizationImpl.passwordReset(userName,newPassword);
         Map<String,Object> response = new HashMap<String,Object>();
         response.put("message",authorizationResult.getMessage());
         response.put("queryStatus", authorizationResult.getAuthorizationStatus());
+        return HttpResponseStatus.setResponse(response);
+    }
+
+    @RequestMapping(value="/getDetails/{userId}", method=RequestMethod.GET)
+    public ResponseEntity<Map<String,Object>> getUserDetails(@PathVariable("userId") int userId) {
+        AuthorizationQueryResult authorizationResult = authorizationImpl.getUserDetails(userId);
+        Map<String,Object> response = new HashMap<String,Object>();
+        response.put("message",authorizationResult.getMessage());
+        response.put("queryStatus", authorizationResult.getAuthorizationStatus());
+        response.put("data",authorizationResult.getData());
         return HttpResponseStatus.setResponse(response);
     }
     
