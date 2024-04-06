606d0dc07e3dbda2103ac12a4cbeff2b12ac178b
Kawshar <kwshr96626@gmail.com>
2024-04-06 05:21:48 -0400

    Add docker file and signup,signin,forgotpasswrd

--- e-auction/src/pages/signin/signin.js
+++ e-auction/src/pages/signin/signin.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/header/header';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './signin.css';
 
 function SignIn() {
   const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage,setErrorMessage] = useState('');
 
-  const handleSignInUser = (event) => {
+  const handleSignInUser = async (event) => {
     event.preventDefault();
-    navigate('/itemsearch');
+    const formData = {
+    userName: userName,
+    password: password
+  }
+    try{
+      const response = await axios.post(`https://authorizationservice-fm4o.onrender.com/signIn`,formData);
+      const status = response.data.status;
+      if(status === 'OK'){
+        navigate('/itemsearch');
+      }
+      else{
+        setUserName('');
+        setPassword('');
+        setErrorMessage('An error occured while logging in: '+response.data.message);
+      }
+    } catch(error){
+      setErrorMessage('An error occured while logging in: '+error);
+    }
   };
 
-  const handleSignInSeller = (event) => {
+  const handleSignInSeller = async (event) => {
     event.preventDefault();
-    navigate('/sellerhome');
+    const formData = {
+    userName: userName,
+    password: password
+  }
+    try{
+      const response = await axios.post(`https://authorizationservice-fm4o.onrender.com/signIn`,formData);
+      const status = response.data.status;
+      if(status === 'OK'){  
+        navigate('/sellerhome');
+      }
+      else{
+        setUserName('');
+        setPassword('');
+        setErrorMessage('An error occured while logging in: ',response.data.message);
+      }
+    } catch(error){
+      setErrorMessage('An error occured while logging in: ',error);
+    }
   };
 
   const handleForgotPassword = () => {
-    console.log('Redirecting to forgot password page');
     navigate('/forgotpassword');
   };
 
@@ -27,12 +64,13 @@ function SignIn() {
       <div className="signin-form-container">
         <h2 className="signin-title">Login</h2>
         <form className="signin-form">
-          <input type="text" id="username" name="username" placeholder="User Name" required />
-          <input type="password" id="password" name="password" placeholder="Password" required />
+          <input type="text" id="username" name="username" placeholder="User Name" required onChange={(e) => setUserName(e.target.value)}/>
+          <input type="password" id="password" name="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)}/>
           <div className="signin-buttons">
             <button onClick={handleSignInUser} className="signin-btn user">Login as USER</button>
             <button onClick={handleSignInSeller} className="signin-btn seller">Login as SELLER</button>
           </div>
+          {errorMessage && <div className = "error-message">{errorMessage}</div>}
         </form>
         <button onClick={handleForgotPassword} className="forgot-password-btn">
           Forgot Password?
