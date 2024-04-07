f55f204efc72ad4391bd2aa02c77f3f0ba4591ea
Yash Dani <yashdani@my.yorku.ca>
2024-04-07 03:32:03 -0400

    added useLocation import to some pages

--- e-auction/src/pages/confirmation/confirmation.js
+++ e-auction/src/pages/confirmation/confirmation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../../components/header/header'; 
 import axios from 'axios'; 
 import './confirmation.css';
