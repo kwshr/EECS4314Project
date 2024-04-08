d266e805f5ddd5c940439b99b58fa154d336821a
Kawshar <kwshr96626@gmail.com>
2024-04-07 23:35:19 -0400

    Modify controller to serve githubio

--- Backend/eauction/AuthorizationService/authorization-service/src/main/java/com/eauction/authorizationservice/AuthorizationController.java
+++ Backend/eauction/AuthorizationService/authorization-service/src/main/java/com/eauction/authorizationservice/AuthorizationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.common.User;
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:3000/*")
+@CrossOrigin(origins = "https://kwshr.github.io/*")
 @RestController
 @RequestMapping
 public class AuthorizationController {
